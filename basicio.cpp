bd4097a54affe09869526c03d81bdc64c539ed71
AnuragMishra88 <anurag.mishraa1210@gmail.com>
2025-08-26 15:23:43 +0530

    UPLOADED

---
+++ basicio.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//! (1) WAP to read your name and concatenate with a student.
+
+// int main() {
+// 	string name;
+// 	getline(cin,name);
+// 	cout<<name<<" "<<"is a student";
+	
+// }
+
+//! (2) WAP to read an integer and add 2 in it
+
+// int main(){
+//     int num;
+//     cin>>num;
+//     cout<<"Number after adding 2: ";
+//     cout<<num+2;
+// }
+
+//! (3) WAP to read name age and roll number and print it in a single line separated by space
+
+// int main(){
+//     string name;
+//     int age;
+//     string roll;
+//     cin>>name>>age>>roll;
+//     cout<<name<<" "<<age<<" "<<roll;
+// }
+
+//! (4) WAP to read 3 integer and multiply it
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a*b*c;
+}
